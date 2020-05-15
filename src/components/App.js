5acae3fb4436401d6425934b4c519dd92baa1c66
StephenLespron <stephen.lespron@gmail.com>
2020-05-15 14:59:15 -0700

    working forum with functional search filter

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,90 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post'
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      posts: []
+      posts: [],
+      filterText: ``
     };
 
     this.updatePost = this.updatePost.bind( this );
     this.deletePost = this.deletePost.bind( this );
     this.createPost = this.createPost.bind( this );
+    this.updateText = this.updateText.bind(this)
+    // this.filterFeed = this.filterFeed.bind(this)
   }
   
   componentDidMount() {
+    axios.get('https://practiceapi.devmountain.com/api/posts')
+         .then(res => {this.setState({posts: res.data})
+                      console.log(res.data)})
+         .catch(err => console.log(err))
+  }
+
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${ id }`, { text })
+         .then(res => {this.setState({posts: res.data})
+                console.log(res.data)})
+         .catch(err => console.log(err))
+  }
+
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+         .then(res => {this.setState({posts: res.data})
+                console.log(res.data)})
+         .catch(err => console.log(err))
+  }
+
+  createPost(text) {
+    axios.post(`https://practiceapi.devmountain.com/api/posts`, {text})
+         .then(res => {this.setState({posts: res.data})})
+         .catch(err => console.log(err))
 
   }
 
-  updatePost() {
-  
+  updateText (val){
+    this.setState({
+      filterText: val
+    })
   }
 
-  deletePost() {
-
-  }
-
-  createPost() {
-
-  }
+  // filterFeed(val) {
+  //   this.setState({
+  //     posts: this.state.posts.filter(elem => this.state.posts[elem].text.includes(val))
+  //   })
+  // }
 
   render() {
     const { posts } = this.state;
-
+    
     return (
       <div className="App__parent">
-        <Header />
+        <Header filterFeedFn = {this.updateText}/>
 
         <section className="App__content">
 
-          <Compose />
+          <Compose createPostFn = {this.createPost} />
+          {/* {posts.map( post => ( */}
+          {posts.filter(elem => elem.text.toLowerCase().includes(this.state.filterText.toLowerCase())).map( post => (
+            <Post key={ post.id }
+            text= { post.text }
+            date= { post.date }
+            id=   {post.id}
+            updatePostFn = {this.updatePost}
+            deletePostFn = {this.deletePost}/>            
+            ))
+          }
           
         </section>
-      </div>
+          </div>
     );
   }
 }
--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -7,14 +7,32 @@ import SearchIcon from 'react-icons/lib/md/search';
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *HEADER* COMPONENT
 
 export default class Search extends Component {
+constructor(){
+  super()
+  this.state ={
+    text: ``
+  }
+}
+
+updateText(val){
+  this.setState({
+    text: val
+  })
+  console.log(this.state.text)
+}
+
+filterFeed(){
+  this.props.filterFeedFn(this.state.text)
+}
+
   render() {
     return (
       <section className="Search__parent">
 
         <div className="Search__content">
-          <input placeholder="Search Your Feed" />
+          <input placeholder="Search Your Feed" onChange = {ev => this.updateText(ev.target.value)} />
 
-          <SearchIcon id="Search__icon" />
+          <SearchIcon id="Search__icon" onClick={() => this.filterFeed()}/>
         </div>
         
       </section>
